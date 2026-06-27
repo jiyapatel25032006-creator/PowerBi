2d5993efb4e34416206b26c3eb62250db7374cd0
jiyapatel25032006-creator <jiyapatel25032006@gmail.com>
2026-06-27 21:06:09 +0530

    Add files via upload

---
+++ index.py
@@ -0,0 +1,10 @@
+print("Jiya")
+print("Ahmedabad")
+print("Welcome to Data Analytics")
+
+
+name = input("My name is:")
+age = int(input("My age is:"))
+
+print("My name is",name "and I am",age "years old")
+
