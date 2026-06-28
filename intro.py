8a72af22fa63ef47fa53b6617cb95698caa0122e
jiyapatel25032006-creator <jiyapatel25032006@gmail.com>
2026-06-28 10:14:33 +0530

    Add files via upload

---
+++ intro.py
@@ -0,0 +1,15 @@
+# print("JIYA")
+# print("25")
+# print("Welcome to Data Analytics")
+
+# name = input("What is your name?")
+# age = int(input("What is your age?"))
+
+# print("My name is",name,"and my age is",age)
+
+# print("My name is",name,"and I am",age,"years old")
+
+Name=input("My name is:")
+Course=input("My course is:")
+Fees=int(input("My fees is:"))
+print("My name is",Name,"and my course is",Course,"and my fees is",Fees)
