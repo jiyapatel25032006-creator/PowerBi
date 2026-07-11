25ff06ab556c1acb256c1cf0daed6f38b5f15405
jiyapatel25032006-creator <jiyapatel25032006@gmail.com>
2026-07-11 16:07:43 +0530

    Add files via upload

---
+++ Loops.py
@@ -0,0 +1,48 @@
+# for Loop//
+# for i in range(5):
+#     print(i)
+
+# for i in range(0,11,2) :
+#     print(i)
+
+# for i in range(10):
+#     print("Namaste Bharat => ", i)
+
+# Loop through Data//  String Example//
+# for i in "Python":
+    # print(i)
+# # List Example//
+# fruits = ["mango","banana","apple"]
+# for fruit in fruits :
+#     print(fruit)
+
+# Nested Loop//
+# for i in range(3) :
+#     for j in range(2) :
+#         print(i,j)
+
+# while Loop//
+# i = 0
+# while i<5 :
+#     print(i)
+#     i+=1
+
+#  Loop Control Statements// break//
+# for i in range(5) :
+#     if i == 3 :
+#         break 
+#     print(i)
+
+# continue//
+# for i in range(5) :
+#     if i == 3 :
+#         continue 
+#     print(i)
+
+# else with Loop//
+# for i in range(5):
+#     if i ==3 :
+#         continue
+#     print(i)
+# else:
+#     print("Finished")
